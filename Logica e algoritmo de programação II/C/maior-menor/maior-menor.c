558086304d34bce92bbc7c54f3fe84c16291dd15
iFallenHunt <lukas_santos.silva@hotmail.com>
2022-07-04 10:54:03 -0300

    removido os exercicios em txt pois ja foram resolvidos

---
+++ Logica e algoritmo de programação II/C/maior-menor/maior-menor.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+int main(){
+	int n1, n2, n3, n4, n5, total;
+	
+	printf("Digite os numeros de entrada: ");
+	scanf("%d", &n1);
+	
+	printf("Digite os numeros de entrada: ");
+	scanf("%d", &n2);
+	
+	printf("Digite os numeros de entrada: ");
+	scanf("%d", &n3);
+	
+	printf("Digite os numeros de entrada: ");
+	scanf("%d", &n4);
+	
+	printf("Digite os numeros de entrada: ");
+	scanf("%d", &n5);
+		
+	
+}
