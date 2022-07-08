c3a1e1a491fb3a75e49c5235ce6e7cc105f83b87
iFallenHunt <lukas_santos.silva@hotmail.com>
2022-07-08 15:56:02 -0300

    adicionado codigo Busca sequencial

---
+++ Logica e algoritmo de programação II/C/exercicios 3 - todos os demais/busca/README.md
@@ -0,0 +1,2 @@
+Usei como base o pseudocodigo do arquivo .txt
+o codigo ainda nao esta pronto mas pelo menos ja esta executando
---
+++ Logica e algoritmo de programação II/C/exercicios 3 - todos os demais/busca/busca-seq.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	int acha, inteiro, num, i;
+	char vnum[20];
+	
+	for (i = 0; i < 20; i++){
+		printf("Informe um numero: ");
+		scanf("%s", &vnum[i]);
+	}
+	printf("\nInforme o numero que deseja buscar: ");
+	printf("%d", &num);
+	i = 1;
+	while ((i < 20) && (acha != 0)){
+		if (num = vnum[i]){
+			acha = 1;
+		}
+		else{
+			i = i + 1;
+		}
+	}
+	if (acha == 1){
+		printf("O elemento foi encontra na posição: \n", i);
+	}
+	else{
+		printf("\nO elemento nao foi encontrado");
+	}
+}
